38edd3ed9d848b5e02bf2583e04b6c9690320562
leexzu1 <jackie20029655@gmail.com>
2024-12-18 15:59:36 +0900

    [feat]Chatting 수정(1218)

--- partylink/src/components/chatting/chatting1.jsx
+++ partylink/src/components/chatting/chatting1.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import * as Styled from "./chatting1.styled";
 
-import LeftQuestion from "../../assets/img/Group 51.svg";
+// import Info1 from "../../assets/img/ghksdudgody.svg";
+import Info2 from "../../assets/img/Group 51.svg";
+// import Info3 from "../../assets/img/rlekflsms.svg";
+// import Info4 from "../../assets/img/slrspdlasladmlthsrkfkrdl.svg";
+// import Info5 from "../../assets/img/10ch.svg";
+// import Info6 from "../../assets/img/ehdwjawk.svg";
 import Question from "../../assets/img/Question.svg";
 import Circle from "../../assets/img/Circle.svg";
 import Arrow from "../../assets/img/Arrow.svg";
@@ -12,13 +17,77 @@ const Chatting1 = () => {
     { text: "Q. 제일 웃음이 많은 사람은?", sender: "player" },
     { text: "Lorem ipsum dolor sit", sender: "player" },
     { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,", sender: "user" },
+    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,", sender: "user" },
+    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,", sender: "user" },
+    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,", sender: "user" },
+    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,", sender: "user" },
+    { text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit,", sender: "user" },
+    { text: "Q. 제일 웃음이 많은 사람은?", sender: "player" },
   ]);
   const [inputValue, setInputValue] = useState("");
 
+  const randomQuestions = [
+    "이번 겨울에 눈싸움 한 사람 접어",
+    "외국에서 길을 잃은 적 있는 사람 접어",
+    "한 번도 비행기 타본 적 없는 사람 접어",
+    "친구의 비밀을 지킨 적 있는 사람 접어",
+    "한 달 이상 운동 안 한 사람 접어",
+    "유명인과 사진 찍은 적 있는 사람 접어",
+    "혼자서 여행 간 적 있는 사람 접어",
+    "24시간 이상 잠을 잔 적 없는 사람 접어",
+    "한 번도 캠핑 간 적 없는 사람 접어",
+    "생일 파티를 매년 해본 사람 접어",
+    "애완동물을 키운 적 있는 사람 접어",
+    "한 번도 콘서트에 간 적 없는 사람 접어",
+    "한 번도 자전거를 타본 적 없는 사람 접어",
+    "가장 좋아하는 영화 장르가 로맨스가 아닌 사람 접어",
+    "한 번도 스키를 타본 적 없는 사람 접어",
+    "친구와 싸운 적 있는 사람 접어",
+    "한 번도 노래방에 간 적 없는 사람 접어",
+    "3개 국어를 할 수 있는 사람 접어",
+    "한 번도 해변에서 일몰을 본 적 없는 사람 접어",
+    "좋아하는 음식을 직접 만들어본 적 있는 사람 접어",
+    "한 번도 마라톤에 참가해본 적 없는 사람 접어",
+    "친구의 생일을 잊어버린 적 있는 사람 접어",
+    "한 번도 도서관에서 책을 빌려본 적 없는 사람 접어",
+    "좋아하는 캐릭터의 굿즈를 가진 적 있는 사람 접어",
+    "한 번도 자원봉사에 참여해본 적 없는 사람 접어",
+    "친구와 단둘이 여행을 가본 적이 없는 사람 접어",
+    "한 번도 핸드폰 없이 하루를 보낸 적 없는 사람 접어",
+    "좋아하는 노래를 부르며 길을 걸어본 적 있는 사람 접어",
+    "한 번도 바다에서 수영해본 적 없는 사람 접어",
+    "친구와의 약속을 한 번도 잊어본 적 없는 사람 접어",
+    "한 번도 고양이를 만져본 적 없는 사람 접어",
+    "생일 케이크를 직접 만들어본 적 있는 사람 접어",
+    "한 번도 해외여행을 가본 적 없는 사람 접어",
+    "한 번도 자전거를 타고 출퇴근해본 적 없는 사람 접어",
+    "본인 지인의 결혼식에 간 적 없는 사람 접어",
+    "한 번도 캠핑장에서 하룻밤을 보낸 적 없는 사람 접어",
+    "한 번도 스스로 요리를 해본 적 없는 사람 접어",
+    "한 번도 바베큐를 해본 적 없는 사람 접어",
+    "좋아하는 스포츠팀의 경기를 한 번도 직접 보지 않은 사람 접어",
+    "지금 딱 생각나는 본인만의 최애 카페가 없는 사람 접어",
+    "좋아하는 음악 장르가 없는 사람 접어",
+    "한 번도 친구와의 대화 중에 거짓말을 한 적 없는 사람 접어",
+    "한 번도 해리포터 시리즈를 읽어본 적 없는 사람 접어",
+    "지금 흰 양말 신고 있는 사람 접어",
+    "부모님과 함께 살고 있는 사람 접어",
+    "더위보다 추위를 많이 타는 사람 접어",
+  ];
+
+  const handleHelpClick = () => {
+    const randomIndex = Math.floor(Math.random() * randomQuestions.length);
+    const randomQuestion = randomQuestions[randomIndex];
+
+    // 입력창에 질문 넣기
+    setInputValue(randomQuestion);
+  };
+
+  // 메시지 입력 이벤트
   const handleSendClick = () => {
     if (inputValue.trim()) {
       setMessages([...messages, { text: inputValue, sender: "user" }]);
-      setInputValue("");
+      setInputValue(""); // 입력창 비우기
     }
   };
 
@@ -27,7 +96,7 @@ const Chatting1 = () => {
       <Styled.Container>
         <Styled.ChatInfo>
           <Styled.Information>
-            <img src={LeftQuestion} alt="닉네임님의 차례입니다. 질문을 남겨주세요." />
+            <img src={Info2} alt="닉네임님의 차례입니다. 질문을 남겨주세요." />
           </Styled.Information>
           <Styled.PlayerInfo>
             <Styled.PlayerLabel>플레이어1</Styled.PlayerLabel>
@@ -44,9 +113,9 @@ const Chatting1 = () => {
             <Styled.QIcon src={Question} alt="QuestionIcon" />
             <Styled.Input type="text" placeholder="대화를 시작해보세요." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
 
-            <Styled.HelpText src={HelpImage} alt="Help" />
+            <Styled.HelpText src={HelpImage} alt="Help" onClick={handleHelpClick} />
 
-            <Styled.IconButtonAll onclick={handleSendClick}>
+            <Styled.IconButtonAll onClick={handleSendClick}>
               <Styled.IconButton1 src={Circle} alt="SendIcon" />
               <Styled.IconButton2 src={Arrow} alt="SendIcon" />
             </Styled.IconButtonAll>
--- partylink/src/components/chatting/chatting1.styled.js
+++ partylink/src/components/chatting/chatting1.styled.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-
 export const ContainerAll = styled.div`
   display: flex;
   justify-content: center;
@@ -7,22 +6,48 @@ export const ContainerAll = styled.div`
   width: 100%;
   height: 100%;
   background-color: #262b34;
+  box-sizing: border-box; /* 여백이 포함된 전체 크기 유지 */
+  padding: 0 10px; /* 좌우 여백 유지 */
 `;
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  width: 90%;
-  max-width: 600px;
+  width: 100%; /* 화면에 맞게 늘어남 */
+  max-width: 600px; /* 최대 너비를 설정 */
   height: 500px;
-  background-color: #262b34;
+  background-color: #1e2224;
   border-radius: 0 0 25px 25px;
+  margin-top: 10px;
+  box-sizing: border-box; /* 패딩과 보더를 너비에 포함 */
 `;
 
 export const ChatInfo = styled.div`
-  padding: 15px;
-  background-color: #262b34;
+  padding: 25px;
+  background-color: #1e2224;
+
+  overflow-y: overlay; /* 스크롤바가 콘텐츠 위에 그려지도록 설정 */
+  overflow-x: hidden; /* 수평 스크롤 숨기기 */
+
+  /* 스크롤바 설정 */
+  &::-webkit-scrollbar {
+    width: 7px;
+  }
+
+  /* 스크롤바 막대 설정 */
+  &::-webkit-scrollbar-thumb {
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 10px;
+    border: 1px solid #1e2224;
+
+    max-height: 10px;
+  }
+
+  /* 스크롤바 뒷 배경 설정 */
+  &::-webkit-scrollbar-track {
+    background-color: #1e2224;
+  }
 `;
 
 export const Information = styled.div`
@@ -32,15 +57,15 @@ export const Information = styled.div`
   margin: 20px 0;
 
   img {
-    width: auto; /* 이미지 크기를 원래 비율대로 유지 */
+    width: 380px;
     max-width: 100%;
   }
 `;
 
-export const PlayerLabel = styled.p`
+export const PlayerLabel = styled.div`
   font-size: 14px;
-  color: #9ca3af; /* 회색 텍스트 */
-  margin-bottom: 10px;
+  color: #848687;
+  // margin-bottom: 10px;
 `;
 
 export const PlayerInfo = styled.div`
@@ -55,7 +80,7 @@ export const Message = styled.div`
   color: #fff;
   margin: 0;
   padding: 10px 15px;
-  background-color: ${(props) => (props.isUser ? "#A1B5A9" : "#A1B5D9")};
+  background: ${(props) => (props.isUser ? "radial-gradient(circle, #c2d5cb 0.1%, #A1B5A9 50%)" : "radial-gradient(circle, #9cabc6 0.1%, #7e90b0 50%)")};
   border-radius: 15px;
   max-width: 80%;
 `;
@@ -64,11 +89,9 @@ export const ChatInputWrapper = styled.div`
   display: flex;
   align-items: center;
   padding: 10px 15px;
-  background-color: #3b3f45;
+  background-color: #1e2224;
   border-radius: 0 0 25px 25px;
   gap: 10px;
-
-  margin-bottom: 20px;
 `;
 
 export const InputContainer = styled.div`
@@ -79,16 +102,22 @@ export const InputContainer = styled.div`
   border-radius: 50px;
   padding: 10px 15px;
   gap: 10px;
+  position: relative;
+
+  margin-bottom: 10px;
 `;
 
 export const QIcon = styled.img`
-  width: 24px;
-  height: 24px;
+  width: 38px;
+  height: 38px;
 `;
 
 export const HelpText = styled.img`
   display: flex;
-  margin-right: 20px;
+  margin-right: 40px;
+
+  width: 35px;
+  height: 20px;
 `;
 
 export const IconButtonAll = styled.div`
@@ -96,11 +125,13 @@ export const IconButtonAll = styled.div`
   position: relative;
   justify-content: center;
   align-items: center;
+
+  right: 16px;
 `;
 
 export const IconButton1 = styled.img`
-  width: 40px; /* Circle 크기 */
-  height: 40px;
+  width: 45px; /* Circle 크기 */
+  height: 45px;
   position: absolute; /* Arrow와 겹치도록 배치 */
   z-index: 1; /* Circle이 뒤로 가도록 설정 */
 `;
@@ -120,8 +151,10 @@ export const Input = styled.input`
   color: #fff;
   outline: none;
 
+  margin-top: 3px;
+
   &::placeholder {
-    color: #9ca3af;
+    color: white;
   }
 `;
 
@@ -129,4 +162,24 @@ export const HelpButton = styled.img`
   width: 30px;
   height: 30px;
   cursor: pointer;
+`;
+
+export const DragButton = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 20px;
+  background-color: #9ca3af;
+  color: #fff;
+  padding: 5px 10px;
+  border-radius: 5px;
+  cursor: pointer;
+  z-index: 10;
+`;
+
+export const MessageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  max-height: 350px; // 메시지가 많아지면 이 높이를 넘지 않도록 설정
+  overflow-y: auto; // 스크롤 가능하도록 설정
 `;
